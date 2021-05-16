92a2e0962d609a6032f2951d86c983074f9e0377
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-05-16 17:09:18 -0500

    -Added update & remove pet menu items

--- PetDB/src/petdb/PetDB.java
+++ PetDB/src/petdb/PetDB.java
@@ -8,11 +8,13 @@ import java.util.Scanner;
  */
 public class PetDB {
     static String[] options = 
-        {"View all pets", 
-        "Add more pets", 
-        "Search pets by name", 
-        "Search pets by age", 
-        "Exit Program"};
+        {"View all pets", // option 1
+        "Add more pets", // option 2
+        "Update an existing pet", // option 3
+        "Remove an existing pet",// option 4
+        "Search pets by name", // option 5
+        "Search pets by age", // option 6
+        "Exit Program"}; // default
     static Menu menu = new Menu(options);
     static Scanner input = new Scanner(System.in);
     static PetList petList = new PetList();
@@ -21,6 +23,8 @@ public class PetDB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Pet Database Program\n");
+        
         do {
             menu.displayMenu();
         } while (select());
@@ -29,7 +33,7 @@ public class PetDB {
     }
     
     
-    static boolean select() {
+    private static boolean select() {
         int option;
         do {
             System.out.print("Your choice: ");
@@ -39,8 +43,10 @@ public class PetDB {
         switch (option) {
             case 1: return petList.printList();
             case 2: return petList.addPets();
-            case 3: return petList.searchPetsByName();
-            case 4: return petList.searchPetsByAge();
+            case 3: return petList.updatePet();
+            case 4: return petList.removePet();
+            case 5: return petList.searchPetsByName();
+            case 6: return petList.searchPetsByAge();
             default: return false;
         }
     }
