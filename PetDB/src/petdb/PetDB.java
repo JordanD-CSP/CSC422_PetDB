9978a7a906049e00e9e95e755ddea40433c9e0b1
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-05-23 18:23:35 -0500

    -added age validation for addPets() and updatePet()

--- PetDB/src/petdb/PetDB.java
+++ PetDB/src/petdb/PetDB.java
@@ -55,6 +55,37 @@ public class PetDB {
         }
     }
     
+    public static boolean addPets() {
+        System.out.println();
+        int petsAdded = 0;
+        do {
+            System.out.print("add pet (name, age): ");
+            String name = input.next().trim();
+            if (name.equals("done")) {
+                break;
+            }
+            int age = input.nextInt();
+            if (age < 0 || age > 20) {
+                System.out.printf("Error: %d is not a valid age.\n", age);
+            } else {
+                petList.add(new Pet(name, age));
+                petsAdded++;
+            }
+        } while (true);
+        System.out.printf("%d pets added.\n", petsAdded);
+        System.out.println();
+        
+        return true;
+    }
+    
+    public static void displayMenu() {
+        int optionNumber = 1;
+        for (String option: options) {
+            System.out.println(optionNumber + ") " + option);
+            optionNumber++;
+        }
+    }
+    
     private static void loadData(){
         File petData = new File("petData.txt");
         if (petData.exists()) {
@@ -70,32 +101,6 @@ public class PetDB {
             } catch (FileNotFoundException ex) {
                 System.out.println(ex);
             }
-        }
-    }
-    
-    private static void saveData(){
-        File petData = new File("petData.txt");
-        try {
-            PrintWriter output = new PrintWriter(petData);
-            String name;
-            int age;
-            for (Pet pet: petList) {
-                name = pet.getName();
-                age = pet.getAge();
-                output.print(name + " " + age + " ");
-            }
-            output.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex);
-        }
-        
-    }
-    
-    public static void displayMenu() {
-        int optionNumber = 1;
-        for (String option: options) {
-            System.out.println(optionNumber + ") " + option);
-            optionNumber++;
         }
     }
     
@@ -159,6 +164,11 @@ public class PetDB {
         return true;
     }
     
+    private static void printTableFooter(int rowCount) {
+        System.out.println("+-------------------------+");
+        System.out.printf("%d rows in set.\n", rowCount);
+    }
+    
     private static void printTableHeader() {
         System.out.println("+-------------------------+");
         System.out.println("|  ID | NAME       |  AGE |");
@@ -169,36 +179,33 @@ public class PetDB {
         System.out.printf("| %3d | %-10s | %4d |\n", id, name, age);
     }
     
-    private static void printTableFooter(int rowCount) {
-        System.out.println("+-------------------------+");
-        System.out.printf("%d rows in set.\n", rowCount);
-    }
-    
-    public static boolean addPets() {
+    public static boolean removePet() {
+        printTable();
         System.out.println();
-        int petsAdded = 0;
-        do {
-            System.out.print("add pet (name, age): ");
-            String name = input.next().trim();
-            if (name.equals("done")) {
-                break;
-            }
-            int age = input.nextInt();
-            petList.add(new Pet(name, age));
-            petsAdded++;
-        } while (true);
-        System.out.printf("%d pets added.\n", petsAdded);
-        System.out.println();
+        System.out.print("Enter the pet ID to remove: ");
+        int id = input.nextInt();
+        System.out.printf("%s %d is removed.\n", 
+                petList.get(id).getName(), petList.get(id).getAge());
+        petList.remove(id);
         
         return true;
     }
-
-    public static boolean searchPetsByName() {
-        System.out.println();
-        System.out.print("Enter a name to search: ");
-        String name = input.next();
-        
-        return printTable(name);
+    
+    private static void saveData(){
+        File petData = new File("petData.txt");
+        try {
+            PrintWriter output = new PrintWriter(petData);
+            String name;
+            int age;
+            for (Pet pet: petList) {
+                name = pet.getName();
+                age = pet.getAge();
+                output.print(name + " " + age + " ");
+            }
+            output.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        }
     }
     
     public static boolean searchPetsByAge() {
@@ -209,30 +216,33 @@ public class PetDB {
         return printTable(age);
     }
     
+    public static boolean searchPetsByName() {
+        System.out.println();
+        System.out.print("Enter a name to search: ");
+        String name = input.next();
+        
+        return printTable(name);
+    }
+    
     public static boolean updatePet() {
         printTable();
         System.out.println();
         System.out.print("Enter the pet ID to update: ");
         int id = input.nextInt();
-        System.out.print("Enter new name and new age: ");
-        String newName = input.next();
-        int newAge = input.nextInt();
+        String newName;
+        int newAge;
+        do {
+            System.out.print("Enter new name and new age: ");
+            newName = input.next();
+            newAge = input.nextInt();
+            if (newAge < 0 || newAge > 20) {
+                System.out.printf("Error: %d is not a valid age.\n", newAge);
+            }
+        } while (newAge < 0 || newAge > 20);
         System.out.printf("%s %d changed to %s %d\n", 
                 petList.get(id).getName(), petList.get(id).getAge(), newName, newAge);
         petList.get(id).setName(newName);
         petList.get(id).setAge(newAge);
-        
-        return true;
-    }
-    
-    public static boolean removePet() {
-        printTable();
-        System.out.println();
-        System.out.print("Enter the pet ID to remove: ");
-        int id = input.nextInt();
-        System.out.printf("%s %d is removed.\n", 
-                petList.get(id).getName(), petList.get(id).getAge());
-        petList.remove(id);
         
         return true;
     }
